d93f487ee5018b9faba13c41e5843a428e27d9f6
Kathrin H0007 <kahe0007@stud.hs-kl.de>
2019-01-16 07:44:48 +0100

    knn sample formatted
    Beispielcode für KNN überarbeitet und formatiert

--- chapters/advancedTopics/src/machinelearning/knnsample.py
+++ chapters/advancedTopics/src/machinelearning/knnsample.py
@@ -5,40 +5,51 @@ from sklearn import datasets
 # Label festlegen
 classLabelVector = ["label1", "label2", "label3"]
 errorCount = 0
-# k-Eingrenzung (hier: auf 5 Nachbarn einschränken)
+# k-Eingrenzung (hier: auf 3 Nachbarn einschränken)
 k = 3
+# Datensätze 0 - 29 werden zum testen von k, die 
+# Datensätze ab Zeile 30 werden zur Klassifikation verwendet
+numTestVectors = 30
 
 
 def classify(inX, dataSet, labels, k):
-    rowCount = dataSet.shape[0]  # Anzahl an Zeilen bestimmen
+    # Anzahl an Zeilen bestimmen
+    rowCount = dataSet.shape[0]
 
-    diffMat = numpy.tile(inX, (rowCount, 1)) - dataSet  # Berechnung der Katheten
-    # (über tile() wird der Eingangsdatensatz über die Zeilenanzahl des dataSet vervielfacht,
-    # der dataSet davon substrahiert)
-
-    sqDiffMat = diffMat ** 2  # Quadrat der Katheten
-    sqDistances = sqDiffMat.sum(axis=1)  # Aufsummieren der Differenzpaare
-    distances = sqDistances ** 0.5  # Quadratwurzel über alle Werte
-    sortedDistIndicies = distances.argsort()  # Aufsteigende Sortierung
+    # Berechnung der Katheten
+    diffMat = numpy.tile(inX, (rowCount, 1)) - dataSet
+    # Quadrat der Katheten
+    sqDiffMat = diffMat ** 2
+    # Aufsummieren der Differenzpaare
+    sqDistances = sqDiffMat.sum(axis=1)
+    # Quadratwurzel über alle Werte
+    distances = sqDistances ** 0.5
+    # Aufsteigende Sortierung
+    sortedDistIndicies = distances.argsort()
 
     classCount = {}
 
     # print("inX = %s, k = %s" % (inX, k))
     # print(sortedDistIndicies)
 
-    for i in range(k):  # Eingrenzung auf k-Werte in der sortierten Liste
-        closest = labels[sortedDistIndicies[i]]  # Label (Kategorie entsprechend der Sortierung aufnehmen
-        classCount[closest] = classCount.get(closest, 0) + 1  # Aufbau eines Dictionary über die
+    # Eingrenzung auf k-Werte in der sortierten Liste
+    for i in range(k):
+        # Label (Kategorie mit Sortierung aufnehmen)
+        closest = labels[sortedDistIndicies[i]]
+        # Aufbau eines Dictionary
+        classCount[closest] = classCount.get(closest, 0) + 1
 
-    sortedClassCount = sorted(classCount, key=classCount.get,
-                              reverse=True)  # Absteigende Sortierung der gesammelten Labels in k-Reichweite
+    # Absteigende Sortierung der Labels in k-Reichweite
     # wobei die Sortierung über den Count (Value) erfolgt
+    sortedClassCount = sorted(classCount, 
+			key=classCount.get, reverse=True)
 
     # print(classCount)
     # print(sortedClassCount[0])
 
-    return sortedClassCount[0]  # Liefere das erste Label zurück
-    # also das Label mit der höchsten Anzahl innerhalb der k-Reichweite
+    # Liefere das erste Label zurück, also das Label
+	# mit der höchsten Anzahl innerhalb der k-Reichweite
+    return sortedClassCount[0]
 
 
 # Daten laden
@@ -47,16 +58,15 @@ dataSet = dt = datasets.load_iris()
 # Anzahl Zeilen
 rowCount = dataSet.size
 
-numTestVectors = 30  # Datensätze 0 - 29 werden zum testen von k verwendet,
-# die Datensätze ab Zeile 30 werden zur Klassifikation verwendet
+# Aufruf des Klassifikators von 0 bis 29
+for i in range(0, numTestVectors):
+    result = classify.classify(dataSet[i, :], 
+		dataSet[numTestVectors:rowCount, :],
+		classLabelVector[numTestVectors:rowCount], k)
 
-
-for i in range(0, numTestVectors):  # Aufruf des Klassifikators von 0 bis 29
-
-    result = classify.classify(dataSet[i, :], dataSet[numTestVectors:rowCount, :],
-                               classLabelVector[numTestVectors:rowCount], k)
-
-    print("%s - the classifier came back with: %s, the real answer is: %s" % (i, result, classLabelVector[i]))
+    print("%s - the classifier came back with: %s, " +
+		"the real answer is: %s" %
+          (i, result, classLabelVector[i]))
 
     if result != classLabelVector[i]:
         errorCount += 1.0
